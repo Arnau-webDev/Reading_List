f4ab1e20f2c80851ad6163041001ced8f9c75591
Arnau-webDev <arnauma16@gmail.com>
2023-08-14 20:00:25 +0200

    Display numberOfPages range filter

--- src/components/BookFilters.tsx
+++ src/components/BookFilters.tsx
@@ -1,16 +1,32 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import { UIBooksContext } from '../context/UIBooksContext';
 
 const BookFilters = () => {
 
+	const [pages, setPages] = useState(0);
 	const { bookGenres } = useContext(UIBooksContext);
+
+	const handleOnChange = ({ target }: React.ChangeEvent<HTMLInputElement>): void => {
+		const { value } = target;
+		setPages(value as unknown as number);
+	};
 
 	return (
 		<div>
-			<label htmlFor="genre">Genres</label>
-			<select name="genre" id="genre">
-				{bookGenres.map((genre) => ( <option key={genre} value={genre}>{genre}</option> ))}
-			</select>
+			<div>
+				<label htmlFor="genre">Genres</label>
+				<select name="genre">
+					{bookGenres.map((genre) => ( <option key={genre} value={genre}>{genre}</option> ))}
+				</select>
+			</div>
+
+			<div>
+				<div>
+					<label htmlFor="numOfPages">Number of Pages</label>
+					<input type="range" name='numOfPages' min={0} max={1200} value={pages} onChange={(e) => handleOnChange(e)}/>
+				</div>
+				<span>{pages}</span>
+			</div>
 		</div>
 	);
 };
